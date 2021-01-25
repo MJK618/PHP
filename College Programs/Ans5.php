7d7c90ece5cdb416cd79d07afbe4cdda74fe7855
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-25 22:02:11 +0530

    Add files via upload

---
+++ College Programs/Ans5.php
@@ -0,0 +1,15 @@
+<?php
+$color=array('white','green','red');{
+foreach($color as $c)
+echo"$c,";
+sort($color);
+}
+foreach($color as $y){
+echo"<li> $y</li>";
+}
+/*
+echo "<pre>";
+print_f($y);
+echo "</pre";
+*/
+?>
